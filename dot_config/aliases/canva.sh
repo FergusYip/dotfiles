e87c0c0719ab6111e5c25461b095d60acc799f5a
Fergus Yip <fergus@canva.com>
2023-06-21 09:44:37 +1000

    Update .config/aliases/canva.sh

--- dot_config/aliases/canva.sh
+++ dot_config/aliases/canva.sh
@@ -7,9 +7,6 @@ lunch() {
     open "https://canv.am/lunch-sydney"
 }
 
-
 alias wp="bin/webpack-dev-server"
 alias sb="bin/storybook_single.sh"
-
-alias fina="fin && git diff --quiet && git diff --cached --quiet"
 
