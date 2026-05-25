65f7ce5470c5383bd06812bed3cf8e73daaabb46
Fergus Yip <fergus@canva.com>
2026-05-26 08:52:16 +1000

    Add .config/nvim/LICENSE Add .config/nvim/README.md Add .config/nvim/.stylua.toml Add .config/nvim/init.lua Remove .config/nvim/init.vim Add .config/nvim/lazy-lock.json Add .config/nvim/lua/autocmds.lua Add .config/nvim/lua/chadrc.lua Add .config/nvim/lua/configs/conform.lua Add .config/nvim/lua/configs/lazy.lua Add .config/nvim/lua/configs/lspconfig.lua Add .config/nvim/lua/mappings.lua Add .config/nvim/lua/options.lua Add .config/nvim/lua/plugins/init.lua

---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "georgeguimaraes/review.nvim",
+    version = "v*",
+    dependencies = {
+      "esmuellert/codediff.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = { "Review" },
+    keys = {
+      { "<leader>r", "<cmd>Review<cr>", desc = "Review" },
+      { "<leader>R", "<cmd>Review commits<cr>", desc = "Review commits" },
+    },
+    opts = {},
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
