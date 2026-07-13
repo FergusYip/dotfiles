f8ed06fc08c6e90a0d86195e0cf73adac58c9c78
Fergus Yip <fergus@canva.com>
2026-07-13 10:07:53 +1000

    Update .config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -7,5 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>gt", "<cmd>GitStatusFast<CR>", { desc = "Git status" })
+map("n", "<leader>fs", "<cmd>Telescope grep_string<CR>", { desc = "grep word under cursor" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
